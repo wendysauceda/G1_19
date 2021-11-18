652f78c79fe2a677b0f43f69bdec97e6b37af5f4
wendysauceda <wdsauceda@unah.hn>
2021-11-17 22:50:11 -0600

    correccion en config_conexion

--- controller/pedidos.php
+++ controller/pedidos.php
@@ -1,5 +1,16 @@
 <?php
- header('Content-Type:aplication/json');
+ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  header('Access-Control-Allow-Origin: *');
+  header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+  header('Access-Control-Allow-Headers: token, Content-Type');
+  header('Access-Control-Max-Age: 1728000');
+  header('Content-Length: 0');
+  header('Content-Type: text/plain');
+  die();
+ }
+ header('Access-Control-Allow-Origin: *');   
+ header('Content-Type:application/json');
+
  require_once("../config/conexion.php");
  require_once("../models/Pedidos.php");
  $pedidos = new Pedidos(); 
@@ -17,11 +28,8 @@
         echo json_encode($datos);
       break;
 
-
-
       case "InsertPedidos":
-        $datos=$pedidos->insert_pedidos($body["ID"],$body["ID_SOCIO"],$body["FECHA_PEDIDO"],$body["DETALLE"],
-        $body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_ENTREGA"],$body["ESTADO"]);
+        $datos=$pedidos->insert_pedidos($body["ID"],$body["ID_SOCIO"],$body["FECHA_PEDIDO"],$body["DETALLE"],$body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_ENTREGA"],$body["ESTADO"]);
         echo json_encode("Pedido Ingresado");
       break;
 
@@ -31,8 +39,7 @@
       break;
 
       case "UpdatePedidos":
-        $datos=$pedidos->Update_Pedido($body["ID"],$body["ID_SOCIO"],$body["FECHA_PEDIDO"],$body["DETALLE"],
-        $body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_ENTREGA"],$body["ESTADO"]);
+        $datos=$pedidos->Update_Pedido($body["ID"],$body["ID_SOCIO"],$body["FECHA_PEDIDO"],$body["DETALLE"],$body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_ENTREGA"],$body["ESTADO"]);
         echo json_encode("Pedido Actualizado");
       break;
 
